346a74d8ee98fa2b01c24ec0cf101fbbd42b06ce
Kirill <89740353+Sergeew-K-V@users.noreply.github.com>
2021-12-12 17:29:02 +0300

    adde modalWindow

---
+++ Tailwind/plugins/modal.js
@@ -0,0 +1,19 @@
+function createModal(options){
+    const modal = document.createElement('div')
+    
+}
+
+
+
+
+$.modal =function(options){
+
+    const modal={
+        open(){
+            
+        },
+        close(){},
+        destroy(){}
+    }
+}
+
